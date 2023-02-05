77dc48afd96df29d7d087b44396601aa1512fa0a
Juan José Revelo <revelojuan88@gmail.com>
2023-02-05 18:57:11 -0500

    Creación proyecto biblioteca

---
+++ libreria/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.inicio, name='inicio'),
+    path('nosotros', views.nosotros, name='nosotros'),
+    path('libros', views.libros, name='libros'),
+    path('crear', views.crear, name='crear'),
+    path('editar', views.editar, name='editar'),
+]
