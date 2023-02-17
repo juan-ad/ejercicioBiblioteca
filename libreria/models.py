c77a9a1b6ae7df7ad8d4b64469286ca5df3c3e25
Juan Revelo <revelojuan88@gmail.com>
2023-02-16 19:56:58 -0500

    Crear y eliminar

--- libreria/models.py
+++ libreria/models.py
@@ -6,3 +6,11 @@ class Libro(models.Model):
     titulo = models.CharField(max_length=100, verbose_name='Título')
     imagen = models.ImageField(upload_to='imagenes/', verbose_name='Imagen', null=True)
     descripcion = models.TextField(verbose_name='Descripcion', null=True)
+
+    def __str__(self):
+        fila = "Título: " + self.titulo + " - " + "Descripción: " + self.descripcion
+        return fila
+    
+    def delete(self, using=None, keep_parents=False):
+        self.imagen.storage.delete(self.imagen.name)
+        super().delete()
--- libreria/views.py
+++ libreria/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Libro
+from .forms import LibroForm
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -10,10 +14,21 @@ def nosotros(request):
     return render(request, 'paginas/nosotros.html')
 
 def libros(request):
-    return render(request, 'libros/index.html')
+    libros = Libro.objects.all() # Traemos todos los datos del objeto
+    return render(request, 'libros/index.html', {'Libros':libros})
 
 def crear(request):
-    return render(request, 'libros/crear.html')
+    formulario = LibroForm(request.POST or None, request.FILES or None)
+
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('libros') 
+    return render(request, 'libros/crear.html', {'formulario': formulario})
 
 def editar(request):
     return render(request, 'libros/editar.html')
+
+def delete(request, pk):
+   record = Libro.objects.get(id = pk)
+   record.delete()
+   return redirect('libros')
